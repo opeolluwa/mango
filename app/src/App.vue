f50426c0eb60021db1ec3d798ad593223f3aa924
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-30 15:44:57 +0100

    impl progress bar

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,35 +5,7 @@
 
   <template v-else>
     <ProcessingState v-show="processingPdf" />
-    <div
-      class="modal absolute bg-black/60 flex flex-col text-accent-secondary w-screen h-screen z-50 px-8"
-      v-show="displayLibrary"
-    >
-      <form action="" class="text-accent-secondary">
-        <div>
-          <label for="price" class="block text-sm/6 font-medium text-gray-900"
-            >Audio books</label
-          >
-          <input
-            type="search"
-            name="price"
-            id="price"
-            class="block min-w-0 grow pr-3 pl-1 text-base text-gray-900 focus:outline-none sm:text-sm/6 border bg-gray-500 w-full rounded-lg py-2 px-6 placeholder:px-4 placeholder:text-accent-secondary border-accent-secondary cursor-pointer"
-            placeholder="search collection"
-          />
 
-          <ul class="mt-2">
-            <li
-              v-for="(audio, index) in audioBooks"
-              :key="index"
-              class="text-sm font-medium overflow-hidden text-ellipsis cursor-pointer"
-            >
-              {{ audio.fileName }}
-            </li>
-          </ul>
-        </div>
-      </form>
-    </div>
     <div
       id="container"
       class="h-screen overflow-hidden bg-secondary bg-[url(cover.jpg)] bg-center-center bg-blend-multiply bg-cover bg-no-repeat bg-accent/60 text-accent-secondary relative"
@@ -67,11 +39,7 @@
             <small>{{ trackDuration | +fancyTimeFormat }}</small>-->
           </div>
 
-          <progress
-            value="32"
-            max="100"
-            class="h-1 flex rounded w-full"
-          ></progress>
+          <Progressbar />
         </div>
 
         <div id="controls" class="flex items-center justify-evenly">
@@ -112,10 +80,9 @@
 </template>
 
 <script lang="ts" setup>
-import Loader from "./components/AppLoader.vue";
 import ProcessingState from "./components/ProcessingState.vue";
 import EmptyState from "./components/EmptyState.vue";
-
+import Progressbar from "./components/ProgressBar.vue"
 import {
   Bars3Icon,
   PlayIcon,
---
+++ app/src/components/ProgressBar.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-white/40 h-1.5 w-full rounded flex flex-col justify-center">
+        <div class="bg-accent-secondary h-1.5 rounded cursor-pointer w-[60%]"></div>
+    </div>
+</template>
+
+
+<script lang="ts" setup></script>
