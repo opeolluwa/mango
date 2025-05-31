4b3ebba425fed8a8fd7a5a47d8107493a79bbe5f
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-06-01 00:35:40 +0100

    impl autio player

--- app/src/components/AudioPlayer.vue
+++ app/src/components/AudioPlayer.vue
@@ -3,45 +3,60 @@
     <div
       class="flex flex-col items-center gap-2 justify-evenly absolute left-0 right-0 bottom-5 mb-4 w-full"
     >
-      <audio ref="audioRef" :src="audioSrc" preload="auto" class="hidden" />
+      <audio
+        ref="audioRef"
+        :src="audioSrc"
+        preload="auto"
+        class="hidden"
+        @timeupdate="updateCurrentTime"
+        @loadedmetadata="updateDuration"
+      />
 
-      <div id="info" class="text-center mb-4">
+      <div id="info" class="text-center mb-24  top-[50%]">
         <p class="leading-loos">You are listening to...</p>
-        <h2 class="text-xl  font-medium overflow-hidden text-ellipsis">
+        <h2 class="text-xl font-medium overflow-hidden text-ellipsis">
           {{ fileName }}
         </h2>
       </div>
 
-      <ProgressBar :progress="10" class="h-1 mb-4" />
-      <div id="controls" class="flex items-center justify-evenly w-full">
+      <div id="controls" class="flex items-center justify-evenly w-2/4">
         <button
-          class="size-8 disabled:text-accent/95 control"
+          class="size-6 disabled:text-accent/95 control"
           aria-label="Previous"
         >
           <BackwardIcon />
         </button>
 
         <button
-          class="bg-accent-secondary text-accent flex justify-center items-center rounded-full size-16 active:scale-75 transition-all duration-75 ease-linear p-[5px]"
+          class="bg-accent-secondary text-accent flex justify-center items-center rounded-full size-12 active:scale-75 transition-all duration-75 ease-linear p-[5px]"
         >
           <PlayIcon
-            class="size-8 transition duration-150 ease-in-out"
+            class="size-6 transition duration-150 ease-in-out"
             v-show="!isPlaying"
             @click="playAudio"
           />
           <PauseIcon
-            class="size-8 transition duration-150 ease-in-out"
+            class="size-6 transition duration-150 ease-in-out"
             v-show="isPlaying"
             @click="pauseAudio"
           />
         </button>
 
         <button
-          class="size-8 disabled:text-accent/95 control"
+          class="size-6 disabled:text-accent/95 control"
           aria-label="Next"
         >
           <ForwardIcon />
         </button>
+      </div>
+
+      <div class="flex justify-between gap-x-2 items-center w-full">
+        <small> {{ formatTime(currentTime) }} </small>
+        <ProgressBar
+          :progress="(currentTime / duration) * 100 || 0"
+          class="mb-[1.2px]"
+        />
+        <small>{{ formatTime(duration) }}</small>
       </div>
     </div>
   </div>
@@ -65,7 +80,8 @@ const props = defineProps<{
 const { fileName, audioSrc } = props;
 const isPlaying = ref(false);
 const audioRef = ref<HTMLAudioElement | null>(null);
-
+const currentTime = ref(0);
+const duration = ref(0);
 const playAudio = () => {
   audioRef.value?.play().catch((err) => {
     console.error("Failed to play audio:", err);
@@ -77,13 +93,34 @@ const pauseAudio = () => {
   audioRef.value?.pause();
   isPlaying.value = !isPlaying.value;
 };
+
+const updateCurrentTime = () => {
+  if (audioRef.value) {
+    currentTime.value = audioRef.value.currentTime;
+  }
+};
+
+const updateDuration = () => {
+  if (audioRef.value) {
+    duration.value = audioRef.value.duration;
+  }
+};
+
+const formatTime = (time: number): string => {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+    2,
+    "0"
+  )}`;
+};
 </script>
 
 <style scoped>
 @reference "../assets/styles.css";
 
 #wrapper > div {
-  @apply px-8 py-3;
+  @apply px-4 py-3;
 }
 
 #wrapper.isActive {
--- app/src/components/ProgressBar.vue
+++ app/src/components/ProgressBar.vue
@@ -1,14 +1,12 @@
 <template>
-  <div
-    class="bg-white/40 h-1.5 w-full rounded-full flex flex-col justify-center"
-  >
+  <div class="bg-white/40 h-1 w-full rounded-full flex flex-col justify-center">
     <div
-      class="bg-accent-secondary h-1.5 rounded-full cursor-pointer"
+      class="bg-accent-secondary h-1 rounded-full cursor-pointer"
       :style="{ width: progress + '%' }"
     ></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps(["progress"]);
+defineProps(["progress"]);
 </script>
