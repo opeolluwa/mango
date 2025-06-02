d030504afa7ed8d4ceac592508fb3228c65c316a
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-06-02 12:26:57 +0100

    add tauri/sqlite, tauri/updater, and tauri/upload

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -24,15 +24,19 @@ pub const LAME_SIDECAR: &str = "lame";
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_upload::init())
+        .plugin(tauri_plugin_single_instance::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
-        // .setup(|app| {
-        //     #[cfg(desktop)]
-        //     let _ = app
-        //         .handle()
-        //         .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}));
-        //     Ok(())
-        // })
+        .setup(|app| {
+            #[cfg(desktop)]
+            let _ = app
+                .handle()
+                .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}));
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             commands::synthesize_audio,
