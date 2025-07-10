54665367bdc5c3a73696d8440acbe7b85b6a0cad
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-07-10 05:16:43 +0100

    fix migration errors

--- server/migrations/20250705203826_create_playlist.sql
+++ server/migrations/20250705203826_create_playlist.sql
@@ -1,8 +1,9 @@
 -- Add migration script here
-CREATE TABLE IF NOT EXISTS
-    playlist (
+CREATE TABLE
+    IF NOT EXISTS playlist (
         identifier UUID NOT NULL PRIMARY KEY,
         name VARCHAR NOT NULL,
-        about VARCHAR created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        updated_at TIMESTAMPTZ DEFAULT NULL,
+        about VARCHAR,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+        updated_at TIMESTAMPTZ DEFAULT NULL
     )
--- server/migrations/20250705204056_update_audio_book_table.sql
+++ server/migrations/20250705204056_update_audio_book_table.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
-ALTER TABLE aduio_book
+ALTER TABLE audio_books
 ADD COLUMN is_loved BOOLEAN DEFAULT false;
 
-ALTER TABLE audio_book
+ALTER TABLE audio_books
 ADD COLUMN playlist_identifier UUID REFERENCES playlist (identifier);
