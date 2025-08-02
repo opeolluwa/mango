1e05cedd0704dcf48cc33660b1a1376553ebf57f
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-08-02 21:57:55 +0100

    feat: reimagine kmp for android,desktop,web, and ios

---
+++ kmp/kmp/composeApp/src/androidMain/kotlin/com/eckko/app/eckko/Platform.android.kt
@@ -0,0 +1,9 @@
+package com.eckko.app.eckko
+
+import android.os.Build
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
