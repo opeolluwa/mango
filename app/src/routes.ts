d43ec50f1017c64039defcfe33e2769d2d836dea
law-al <lawahm303@gmail.com>
2025-08-05 17:28:10 +0100

    feat(ui): added the verify account view

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,92 +1,99 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
-import AudioPlayer from "@views/player/AudioPlayer.vue";
-import Favourites from "@views/app/FavouritesPage.vue";
-import History from "@views/app/HistoryPage.vue";
-import Home from "@views/app/HomePage.vue";
-import AppLayout from "@views/app/layout.vue";
-import Library from "@views/app/LibraryPage.vue";
-import Notification from "@views/app/NotificationPage.vue";
-import Settings from "@views/app/SettingsPage.vue";
-import Voices from "@views/app/VoicesPage.vue";
+import AudioPlayer from '@views/player/AudioPlayer.vue';
+import Favourites from '@views/app/FavouritesPage.vue';
+import History from '@views/app/HistoryPage.vue';
+import Home from '@views/app/HomePage.vue';
+import AppLayout from '@views/app/layout.vue';
+import Library from '@views/app/LibraryPage.vue';
+import Notification from '@views/app/NotificationPage.vue';
+import Settings from '@views/app/SettingsPage.vue';
+import Voices from '@views/app/VoicesPage.vue';
 
-import ConfirmOtpPage from "./views/authentication/ConfirmOtpPage.vue";
-import ForgottenPassword from "./views/authentication/ForgottenPassword.vue";
-import AuthenticationLayout from "./views/authentication/layout.vue";
-import LoginPage from "./views/authentication/LoginPage.vue";
-import LoginExistingPage from "./views/authentication/LoginExistingPage.vue";
-import SetNewPassword from "./views/authentication/SetNewPassword.vue";
-import SignupPage from "./views/authentication/SignupPage.vue";
+import ConfirmOtpPage from './views/authentication/ConfirmOtpPage.vue';
+import ForgottenPassword from './views/authentication/ForgottenPassword.vue';
+import AuthenticationLayout from './views/authentication/layout.vue';
+import LoginPage from './views/authentication/LoginPage.vue';
+import LoginExistingPage from './views/authentication/LoginExistingPage.vue';
+import SetNewPassword from './views/authentication/SetNewPassword.vue';
+import SignupPage from './views/authentication/SignupPage.vue';
+import OnboardingPage from './views/authentication/OnboardingPage.vue';
+import VerifyAccountRecovery from './views/authentication/VerifyAccountRecovery.vue';
 
-import ScreenOne from "./views/walkthrough/ScreenOne.vue";
-import ScreenTwo from "./views/walkthrough/ScreenTwo.vue";
-import ScreenThree from "./views/walkthrough/ScreenThree.vue";
+import ScreenOne from './views/walkthrough/ScreenOne.vue';
+import ScreenTwo from './views/walkthrough/ScreenTwo.vue';
+import ScreenThree from './views/walkthrough/ScreenThree.vue';
 
-import AudioPlayerLayout from "./views/player/layout.vue";
-import OnboardingPage from "./views/authentication/OnboardingPage.vue";
+import AudioPlayerLayout from './views/player/layout.vue';
+
 const routes = [
   {
-    path: "/",
-    name: "ScreenOne",
+    path: '/',
+    name: 'ScreenOne',
     component: ScreenOne,
   },
   {
-    path: "/screen-two",
-    name: "ScreenTwo",
+    path: '/screen-two',
+    name: 'ScreenTwo',
     component: ScreenTwo,
   },
   {
-    path: "/screen-three",
-    name: "ScreenThree",
+    path: '/screen-three',
+    name: 'ScreenThree',
     component: ScreenThree,
   },
   {
-    path: "/app",
-    name: "AppLayout",
+    path: '/app',
+    name: 'AppLayout',
     component: AppLayout,
     children: [
-      { path: "", component: Home, name: "Home" },
-      { path: "favourites", component: Favourites, name: "Favourites" },
-      { path: "library", component: Library, name: "Library" },
-      { path: "recent", component: History, name: "Recent" },
-      { path: "voices", component: Voices, name: "Voices" },
-      { path: "settings", component: Settings, name: "Settings" },
-      { path: "notification", component: Notification, name: "Notification" },
+      { path: '', component: Home, name: 'Home' },
+      { path: 'favourites', component: Favourites, name: 'Favourites' },
+      { path: 'library', component: Library, name: 'Library' },
+      { path: 'recent', component: History, name: 'Recent' },
+      { path: 'voices', component: Voices, name: 'Voices' },
+      { path: 'settings', component: Settings, name: 'Settings' },
+      { path: 'notification', component: Notification, name: 'Notification' },
     ],
   },
   {
-    path: "/player",
-    name: "AppPlayer",
+    path: '/player',
+    name: 'AppPlayer',
     component: AudioPlayerLayout,
-    children: [{ path: "", component: AudioPlayer, name: "Player" }],
+    children: [{ path: '', component: AudioPlayer, name: 'Player' }],
   },
   {
-    path: "/auth",
-    name: "AuthenticationLayout",
+    path: '/auth',
+    name: 'AuthenticationLayout',
     component: AuthenticationLayout,
     children: [
-      { path: "", component: SignupPage, name: "SignUp" },
-      { path: "signup", component: SignupPage, name: "SignUp" },
-      { path: "login", component: LoginPage, name: "Login" },
+      { path: '', component: SignupPage, name: 'SignUp' },
+      { path: 'signup', component: SignupPage, name: 'SignUp' },
+      { path: 'login', component: LoginPage, name: 'Login' },
       {
-        path: "login2",
+        path: 'login2',
         component: LoginExistingPage,
-        name: "ExistingUserLogin",
+        name: 'ExistingUserLogin',
       },
       {
-        path: "forgotten-password",
+        path: 'forgotten-password',
         component: ForgottenPassword,
-        name: "ForgottenPassword",
+        name: 'ForgottenPassword',
       },
-      { path: "confirm-otp", component: ConfirmOtpPage, name: "ConfirmOtp" },
+      { path: 'confirm-otp', component: ConfirmOtpPage, name: 'ConfirmOtp' },
       {
-        path: "set-new-password",
+        path: 'verify-account',
+        component: VerifyAccountRecovery,
+        name: 'verifyAccountRecovery',
+      },
+      {
+        path: 'set-new-password',
         component: SetNewPassword,
-        name: "SetNewPassword",
+        name: 'SetNewPassword',
       },
       {
-        path: "onboarding",
-        name: "Onboarding",
+        path: 'onboarding',
+        name: 'Onboarding',
         component: OnboardingPage,
       },
     ],
