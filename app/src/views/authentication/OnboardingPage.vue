2431f1570be45e27daf81eb854352a920a816307
law-al <lawahm303@gmail.com>
2025-08-05 09:24:04 +0100

    feat: add onboarding form submission to backend

--- app/src/views/authentication/OnboardingPage.vue
+++ app/src/views/authentication/OnboardingPage.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="layout">
-    <AuthScreenHeaderText>Onboard to get started</AuthScreenHeaderText>
-    <p class="small text-gray-400">Register a free account to get started!</p>
+    <AuthScreenHeaderText>Complete your profile</AuthScreenHeaderText>
+    <p class="small text-gray-400">Just a few details to get you started</p>
 
     <form @submit="onSubmit" class="mt-8 flex flex-col gap-y-8">
+      <ErrorOutlet v-if="formSubmitError">{{ formSubmitError }}</ErrorOutlet>
+
       <div class="flex flex-col w-full">
         <AppFormLabel for="firstname" text="Firstname" />
         <input
@@ -32,7 +34,7 @@
         <ErrorOutlet v-if="errors.firstname">{{ errors.lastname }}</ErrorOutlet>
       </div>
 
-      <SubmitButton />
+      <SubmitButton :loading="processingRequest" />
     </form>
   </div>
 </template>
@@ -45,6 +47,12 @@
 
   import * as yup from 'yup';
   import ErrorOutlet from '../../components/form/ErrorOutlet.vue';
+  import axios from '../../axios.config';
+  import { useRoute, useRouter } from 'vue-router';
+  import { ref } from 'vue';
+
+  const route = useRoute();
+  const router = useRouter();
 
   const formSchema = yup.object({
     firstname: yup.string().required(),
@@ -55,10 +63,36 @@
     validationSchema: formSchema,
   });
 
-  const onSubmit = handleSubmit((values) => {
-    console.log(values);
-  });
-
   const [firstname, firstnameAttr] = defineField('firstname');
   const [lastname, lastnameAttr] = defineField('lastname');
+  const formSubmitError = ref('');
+  const processingRequest = ref(false);
+
+  const onSubmit = handleSubmit(async (values) => {
+    processingRequest.value = true;
+    try {
+      const { firstname, lastname } = values;
+      const { token } = route.params;
+
+      const response = await axios.post(
+        '/auth/onnboard',
+        { firstname, lastname },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        router.push({ name: 'Home' });
+      } else {
+        formSubmitError.value = response.data.error || 'Failed';
+      }
+    } catch (error: any) {
+      formSubmitError.value = error.response.data.message;
+    } finally {
+      processingRequest.value = false;
+    }
+  });
 </script>
