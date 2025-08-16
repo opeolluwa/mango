bb0962772b686aaaf605d64508b28d6f859098b8
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-08-16 14:40:08 +0100

    update

--- server/src/services/notification_service.rs
+++ server/src/services/notification_service.rs
@@ -5,9 +5,14 @@ use axum::{
 use sqlx::{Pool, Postgres};
 
 use crate::{
-    adapters::notification::CreateNotification, errors::service_error::ServiceError,
-    repositories::notification_repository::NotificationRepository,
-    repositories::notification_repository::NotificationRepositoryExt,
+    adapters::notification::CreateNotification,
+    entities::notifications::Notification,
+    errors::service_error::ServiceError,
+    repositories::notification_repository::{NotificationRepository, NotificationRepositoryExt},
+};
+use futures_util::{
+    sink::SinkExt,
+    stream::{SplitSink, SplitStream, StreamExt},
 };
 
 #[derive(Clone)]
@@ -22,20 +27,27 @@ impl NotifiactionService {
         }
     }
     pub async fn handle_web_socket_connection(&self, mut socket: WebSocket) {
-        let msg = Message::text(
-            r#"
-        {
-  "playlist_identifier": null,
-  "file_name": "nccf-letter-draft-2.docx",
-  "user_identifier": "a2d4eb25-5b46-44ed-8bb7-281e568be7a5",
-  "file_path": "/tmp/1755344187_nccf-letter-draft-2.docx.pdf"
-}"#,
-        );
-        if socket.send(msg).await.is_err() {
-            // client disconnected
-            return;
-        }
+        let (mut sender, mut receiver) = socket.split();
+
+        //         let msg = Message::text(
+        //             r#"
+        //         {
+        //   "playlist_identifier": null,
+        //   "file_name": "nccf-letter-draft-2.docx",
+        //   "user_identifier": "a2d4eb25-5b46-44ed-8bb7-281e568be7a5",
+        //   "file_path": "/tmp/1755344187_nccf-letter-draft-2.docx.pdf"
+        // }"#,
+        //         );
+        //         if socket.send(msg).await.is_err() {
+        //             // client disconnected
+        //             return;
+        //         }
+
+        tokio::spawn(Self::read_message_from_worker(receiver));
+        tokio::spawn(Self::send_mesage_to_client(sender));
     }
+    pub async fn read_message_from_worker(receiver: SplitStream<WebSocket>) {}
+    pub async fn send_mesage_to_client(sender: SplitSink<WebSocket, Message>) {}
 
     //push message to client
     pub async fn notify() {}
@@ -48,6 +60,9 @@ pub trait NotificationServiceExt {
     ) -> impl std::future::Future<Output = Result<(), ServiceError>> + Send;
 
     fn listen_for_new_notifications(&self) -> impl std::future::Future<Output = Response> + Send;
+
+    fn get_latest_unred_notifications()
+    -> impl std::future::Future<Output = Result<Vec<Notification>, ServiceError>> + Send;
 }
 
 impl NotificationServiceExt for NotifiactionService {
@@ -62,5 +77,9 @@ impl NotificationServiceExt for NotifiactionService {
     async fn listen_for_new_notifications(&self) -> Response {
         todo!()
         // self.handle_web_socket_connection(socket).await
+    }
+
+    async fn get_latest_unred_notifications() -> Result<Vec<Notification>, ServiceError> {
+        todo!()
     }
 }
