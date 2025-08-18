832fb433d84f18dd515cd2700a53c0d3bce4de2c
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-08-18 13:08:54 +0100

    feat

--- app/src/axios.config.ts
+++ app/src/axios.config.ts
@@ -11,6 +11,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://eckko.koyeb.app";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Accept"] = "application/json";
-axios.defaults.timeout = 10000; // Set a timeout of 10 seconds
+// axios.defaults.timeout = 10000; // Set a timeout of 10 seconds
 
 export default axios;
