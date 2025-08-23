62f1e48a6cdbd8ce63c557bcf349d426db2521ed
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-08-23 01:17:22 +0100

    update

--- server/src/entities/audio_book.rs
+++ server/src/entities/audio_book.rs
@@ -2,17 +2,25 @@ use chrono::DateTime;
 use chrono::Local;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
+use ts_rs::TS;
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize, FromRow, Default)]
+#[derive(Debug, Serialize, Deserialize, FromRow, Default, TS)]
 #[serde(rename_all = "camelCase")]
+#[ts(export, export_to = "audioBook.d.ts")]
 pub struct AudioBookEntity {
+    #[ts(type = "string")]
     pub identifier: Uuid,
     pub name: String,
     pub src: String,
+    #[ts(type = "string")]
     pub user_identifier: Uuid,
+    #[ts(type = "string")]
     pub playlist_identifier: Option<Uuid>,
+    #[ts(type = "date")]
     pub created_at: DateTime<Local>,
+    #[ts(type = "date")]
     pub updated_at: Option<DateTime<Local>>,
+    #[ts(type = "date")]
     pub last_played: Option<DateTime<Local>>,
 }
