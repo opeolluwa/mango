af8229e666f1e54271cef3402e0f2e48422d2eeb
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-08-26 19:16:08 +0100

    imple password update

--- server/src/routes/users_router.rs
+++ server/src/routes/users_router.rs
@@ -1,11 +1,14 @@
 use axum::{
     Router,
-    routing::{get, post, put},
+    routing::{get, patch, post, put},
 };
 
 use crate::{
-    handlers::users_handlers::{
-        retrieve_information, update_password, update_profile, update_profile_picture,
+    handlers::{
+        authentication_handlers::change_password,
+        users_handlers::{
+            retrieve_information, update_password, update_profile, update_profile_picture,
+        },
     },
     states::services_state::ServicesState,
 };
@@ -16,5 +19,6 @@ pub(super) fn user_routes(state: ServicesState) -> Router {
         .route("/profile", put(update_profile))
         .route("/avatar", post(update_profile_picture))
         .route("/password", put(update_password))
+        .route("/password", patch(change_password))
         .with_state(state)
 }
