3f5bcd10f2e310edf4e9132e584556978bd87b91
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-08-29 00:19:14 +0100

    new build

---
+++ app/src/plugins/revenueCat.ts
@@ -0,0 +1,7 @@
+
+
+    // const appUserId = authentication.getAppUserId(); // Replace with your own authentication system
+    // const purchases = Purchases.configure({
+    //     apiKey: WEB_BILLING_PUBLIC_API_KEY,
+    //     appUserId: appUserId,
+    // });
